c6e9ac6c86d00234496c08dc01d5179aad0499e7
Jonathan Tri <jonathantric@gmail.com>
2022-06-23 08:31:54 +0700

    Initial Commit

---
+++ AnimatedApp/AnimatedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedAppApp.swift
+//  AnimatedApp
+//
+//  Created by Jonathan Tri Christianto on 23/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimatedApp
+//
+//  Created by Jonathan Tri Christianto on 23/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
